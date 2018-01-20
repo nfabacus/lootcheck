0166bb2e8fc36eacb9342ac69c0de64a69ed89a9
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2018-01-20 09:48:44 +0000

    adds tests and implementation of deposit and withdraw

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,10 +1,24 @@
 import balanceReducer from './balance'
 import * as constants from '../actions/constants'
 
+let balance
 describe('balanceReducer', ()=>{
+  beforeEach(()=>{
+    balance = 10
+  })
+
   it('sets a balance', ()=>{
-    const balance = 10
 
     expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance})).toEqual(balance)
+  })
+
+  it('deposits an amount to the balance', ()=>{
+    const deposit = 5
+    expect(balanceReducer(balance, { type: constants.DEPOSIT, deposit})).toEqual(balance+deposit)
+  })
+
+  it('withdraw an amount from the balance', ()=>{
+    const withdrawal = 3
+    expect(balanceReducer(balance, { type: constants.WITHDRAW, withdrawal})).toEqual(balance-withdrawal)
   })
 })
