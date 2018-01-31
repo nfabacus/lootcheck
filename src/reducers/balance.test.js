fd96619b27bb4e3f2099b025d621bc68f7d91022
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2018-01-31 06:10:53 +0000

    storing data in cookie

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,24 +1,34 @@
 import balanceReducer from './balance'
+import balanceReducer2 from './balance'
 import * as constants from '../actions/constants'
 
-let balance
+
 describe('balanceReducer', ()=>{
-  beforeEach(()=>{
-    balance = 10
+
+  describe('When initialising', ()=>{
+    const balance = 10
+
+    it('sets a balance', ()=>{
+      expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance})).toEqual(balance)
+    })
+  
+    describe('then re-initialising', ()=>{
+      it('reads the balance from cookies', ()=>{
+        expect(balanceReducer2(undefined, {})).toEqual(balance)
+      })
+    })
   })
 
-  it('sets a balance', ()=>{
-
-    expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance})).toEqual(balance)
-  })
 
   it('deposits an amount to the balance', ()=>{
-    const deposit = 5
-    expect(balanceReducer(balance, { type: constants.DEPOSIT, deposit})).toEqual(balance+deposit)
+    const initialState = 5
+    const deposit = 10
+    expect(balanceReducer(initialState, { type: constants.DEPOSIT, deposit})).toEqual(initialState+deposit)
   })
 
   it('withdraw an amount from the balance', ()=>{
-    const withdrawal = 3
-    expect(balanceReducer(balance, { type: constants.WITHDRAW, withdrawal})).toEqual(balance-withdrawal)
+    const initialState = 10
+    const withdrawal = 6
+    expect(balanceReducer(initialState, { type: constants.WITHDRAW, withdrawal})).toEqual(initialState-withdrawal)
   })
 })
